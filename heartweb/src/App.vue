eb8b0af7dd577789c6a4a18aa014290695c0d65b
Tee <ttp.tee@hotmail.com>
2020-10-23 15:14:23 +0700

    add font

--- heartweb/src/App.vue
+++ heartweb/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from '@/views/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "Prompt";
+  src: local("Prompt"),
+   url(./assets/font/Prompt/Prompt-Light.ttf) format("truetype");
+}
+*{
+font-family: "Prompt";
+
 }
 </style>
---
+++ heartweb/src/views/Main.vue
@@ -0,0 +1,49 @@
+<template>
+<div>    
+    <div class="Header">
+        Heart Rate Result
+    </div>
+    <button class="StartBtn">
+        Start
+    </button>
+</div>
+
+</template>
+
+<script>
+import * as firebase from 'firebase'
+var config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',      
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+}
+firebase.initializeApp(config)
+export default {
+    
+}
+</script>
+
+<style>
+body{
+    background-color: cadetblue;
+}
+.Header{
+    text-align: center;
+    font-size: 50px;
+    color: aliceblue;
+}
+.StartBtn{
+    position: absolute;
+    width: 30%;
+    bottom: 10%;
+    height: 150px;
+    font-size: 30px;
+    left: 35%;
+
+}
+
+
+</style>
